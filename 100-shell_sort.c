92fe3bee7974793cd9034d8b7bd43d4dc243472c
SebasGTX1 <sebas-0202@hotmail.es>
2022-06-20 23:11:09 -0500

    Fixing negatives case and fixing the logic

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,9 +9,9 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	int gap = 1, i = 0, temp = 0;
+	int gap = 1, i = 0, temp = 0, j = 0;
 
-	if (array == NULL || size < 2)
+	if (!array || size < 2)
 		return;
 	/* max gap calculation */
 
@@ -21,15 +21,16 @@ void shell_sort(int *array, size_t size)
 	/* sorting patron*/
 	while (gap > 0)
 	{
-		for (i = 0; i < (int)size; i++)
+		for (i = gap; i < (int)size; i++)
 		{
-			if ((i + gap) < (int)size && array[i] > array[i + gap])
+			temp = array[i];
+			j = i;
+			while (j > gap - 1 && array[j - gap] > temp)
 			{
-				temp = array[i];
-				array[i] = array[i + gap];
-				array[i + gap] = temp;
-				i = 0;
+				array[j] = array[j - gap];
+				j -= gap;
 			}
+			array[j] = temp;
 		}
 	gap /= 3;
 	print_array(array, size);
